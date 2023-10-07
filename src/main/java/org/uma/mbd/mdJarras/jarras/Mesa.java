beacfc8ed803329a1d5850d3ad067e64b4d125bb
Francisco Morales <franmobe00@gmail.com>
2023-10-07 11:58:53 +0200

    Add Mesa.java

---
+++ src/main/java/org/uma/mbd/mdJarras/jarras/Mesa.java
@@ -0,0 +1,53 @@
+package org.uma.mbd.mdJarras.jarras;
+
+public class Mesa {
+
+    private Jarra jarraA, jarraB;
+
+    public Mesa(int capacidadA, int capacidadB){
+        jarraA = new Jarra(capacidadA);
+        jarraB = new Jarra(capacidadB);
+    }
+
+    public void llenaA(){
+        jarraA.llena();
+    }
+    public void llenaB(){
+        jarraB.llena();
+    }
+    public void vaciaA(){
+        jarraA.vacia();
+    }
+    public void vaciaB(){
+        jarraB.vacia();
+    }
+
+    public void vuelcaASobreB(){
+        jarraB.llenaDesde(jarraA);
+    }
+    public void vuelcaBSobreA(){
+        jarraA.llenaDesde(jarraB);
+    }
+
+    public int getContenidoA(){
+        return jarraA.getContenido();
+    }
+    public int getContenidoB(){
+        return jarraB.getContenido();
+    }
+    public int getCapacidadA(){
+        return jarraA.getCapacidad();
+    }
+    public int getCapacidadB(){
+        return jarraB.getCapacidad();
+    }
+
+    public int getContenido(){
+        return jarraA.getContenido() + jarraB.getContenido();
+    }
+
+    @Override
+    public String toString(){
+        return "JarraA("+jarraA+"), JarraB("+jarraB+")";
+    }
+}
