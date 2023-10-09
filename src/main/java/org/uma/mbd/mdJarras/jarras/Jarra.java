ea23d9ea896a24574b33d93fe98148b40a7b6649
Francisco Morales <franmobe00@gmail.com>
2023-10-09 20:25:17 +0200

    Fix: Changes in MainNPI and Jarra protect to exceptions

--- src/main/java/org/uma/mbd/mdJarras/jarras/Jarra.java
+++ src/main/java/org/uma/mbd/mdJarras/jarras/Jarra.java
@@ -2,10 +2,13 @@ package org.uma.mbd.mdJarras.jarras;
 
 public class Jarra {
 
-    private int capacidad;
+    private final int capacidad;
     private int contenido;
 
     public Jarra(int capInicial){
+        if (capInicial <= 0)
+            throw new IllegalArgumentException("No se puede crear una Jarra con capacidad " + capInicial);
+
         capacidad = capInicial;
         this.vacia();
     }
@@ -20,6 +23,11 @@ public class Jarra {
 
     public void llenaDesde(Jarra jarra_de_llenado) {
 
+        if (this == jarra_de_llenado)
+            throw new IllegalArgumentException("No se puede volcar una jarra sobre si misma");
+
+        // Objects.requireNonNull(jarra_de_llenado, "La Jarra no existe");
+        // Clase Objects ayuda con excepciones.
         int cabe = capacidad - contenido;
         if (cabe >= jarra_de_llenado.contenido) {
             contenido += jarra_de_llenado.contenido;
--- src/main/java/org/uma/mbd/mdNPI/Main.java
+++ src/main/java/org/uma/mbd/mdNPI/Main.java
@@ -5,31 +5,20 @@ public class Main {
     public static void main(String[] args) {
         NPI cpi = new NPI();
 
-        System.out.println("** SIMULADOR DE CALCULADORA CON NOTACION POLACA INVERSA **");
+        System.out.println("** CALCULADORA CON NOTACION POLACA INVERSA **");
         // Resolver la expresión: 3 * (6 - 2) + (2 + 7) / 5 -> en NPI: 3 6 2 - * 5 2 7 + / +
-        System.out.println("Resolucion de 3 6 2 - * 5 2 7 + / + ");
+        System.out.println("Calculo de 3 6 2 - * 5 2 7 + / + ");
         cpi.entra(3);
-        System.out.println(cpi);
         cpi.entra(6);
-        System.out.println(cpi);
         cpi.entra(2);
-        System.out.println(cpi);
         cpi.resta();
-        System.out.println(cpi);
         cpi.multiplica();
-        System.out.println(cpi);
         cpi.entra(5);
-        System.out.println(cpi);
         cpi.entra(2);
-        System.out.println(cpi);
         cpi.entra(7);
-        System.out.println(cpi);
         cpi.suma();
-        System.out.println(cpi);
         cpi.divide();
-        System.out.println(cpi);
         cpi.suma();
-        System.out.println(cpi);
-        System.out.println(" Resultado = " + cpi.getResultado());
+        System.out.println("Resultado = " + cpi.getResultado());
     }
 }
