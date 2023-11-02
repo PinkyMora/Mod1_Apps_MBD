effdbeba2fdddc74788c35a71242c22e3e0d3e88
Francisco Morales <franmobe00@gmail.com>
2023-11-02 18:27:57 +0100

    fix: Change hashCode Pareja

--- src/main/java/org/uma/mbd/mdAmigoInvisible/amigos/Pareja.java
+++ src/main/java/org/uma/mbd/mdAmigoInvisible/amigos/Pareja.java
@@ -11,7 +11,7 @@ public record Pareja(Persona uno, Persona otro) {
 
     @Override
     public int hashCode() {
-        return Objects.hash(uno.getNombre().toLowerCase(), otro.getNombre().toLowerCase());
+        return Objects.hash(uno.getNombre().toLowerCase(), otro.getNombre().toLowerCase()) + Objects.hash(otro.getNombre().toLowerCase(), uno.getNombre().toLowerCase());
     }
 }
 
